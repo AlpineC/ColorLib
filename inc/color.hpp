5d642eb184850340e6446ec2d87f0bffa09a3978
AlpineC <doeshie3@gmail.com>
2025-04-13 20:19:45 +0200

    Added some basic functionality

--- inc/color.hpp
+++ inc/color.hpp
@@ -1,18 +1,44 @@
 #include <iostream>
 
-#define ESCAPE "\033["
-#define RESET
-#define BLUE 106
-#define RED
-#define PURPLE
+#define START "\033["
+#define ESCAPE "\033[0m"
+#define BLUE 34
+#define RED 31
+#define Magneta 35
+
 
 class ColourString
 {
+    
   
     public:
+    std::string value;
+    int color{0};
+
     ColourString(std::string string, int colour)
     {
-       
+       color = colour;
+       value = START + std::to_string(color) + "m" + string + ESCAPE;
     }
 
+    void ChangeColor(int NewColor)
+    {
+        color = NewColor;
+    }
+
+};
+
+bool IsColor(ColourString String, int color)
+{
+   if (color == String.color)
+   {
+     return true;
+   }
+
+   else
+   {
+    return false;
+   }
+   
 }
+
